8526ce5418508a6efaae4e18e9addb5296346805
hongdanh158 <lthdanh@gmail.com>
2017-12-06 23:19:47 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,116 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ArtArt Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header class="header">
+      <!-- Top menu -->
+      <div class="top-menu">
+        <div class="container">
+          <div class="row mr-auto ">
+            <div class="promotion col-lg-8 align-middle">
+              <a href="#">Chương trình khuyến mại tháng 11 Sale cực khủng</a>
+            </div>
+            <div class="col-lg-4 navbar navbar-expand-md">
+              <div class="top-right-menu collapse navbar-collapse d-flex flex-row-reverse" id="navbarsExampleDefault">
+                <ul class="nav nav-pills">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Danh sách điểm bán hàng</a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Tài khoản</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Top menu -->
+      <!-- Navbar-->
+        <nav class="main-nav">
+          <div class="container">
+            <a class="navbar-brand hidden-text" href="#">Navbar</a>
+            <div class="shopping-cart float-right">
+              <a href="#">
+                <img src="images/icon-shopping-cart.png" alt="">
+                <span class="badge badge-light">4</span>
+              </a>
+            </div>
+            <input class="form-control search float-right" type="text" placeholder="Tìm kiếm sản phẩm..." aria-label="Search">
+            <ul class="nav nav-pills float-right">
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mua hàng</a>
+                  <ul class="dropdown-menu" aria-labelledby="dropdown01">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                  </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Thương hiệu</a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="#">Ứng dụng</a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="#">Kiến thức</a>
+              </li>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+            </ul>
+        </div>
+        </nav>
+      <!-- End Navbar -->
+    </header>
+    <main role="main" class="container">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="images/ex/slider-1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/ex/slider-2.jpg" alt="First slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </main><!-- /.container -->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
