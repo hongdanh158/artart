65ffa77c5b5de63d9f648e63d17756dee4bac984
hongdanh158 <lthdanh@gmail.com>
2017-12-07 16:14:20 +0700

    UPdate

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
   </head>
 
   <body>
@@ -43,7 +45,9 @@
       <!-- Navbar-->
         <nav class="main-nav">
           <div class="container">
-            <a class="navbar-brand hidden-text" href="#">Navbar</a>
+            <a class="navbar-brand hidden-text" href="#">
+              <img src="images/logo.jpg" alt="">
+            </a>
             <div class="shopping-cart float-right">
               <a href="#">
                 <img src="images/icon-shopping-cart.png" alt="">
@@ -102,10 +106,340 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
+      <div class="section home-news">
+        <h2 class="title">CẢM HỨNG SÁNG TẠO MỚI</h2>
+        <div class="home-text">Hãy cùng ArtArt tham gia vào cuộc phiêu lưu khám phá chất liệu sáng tạo mới, để mỗi ngày của bạn đều luôn tươi mới và đầy ắp sự sáng tạo.</div>
+        <div class="row list-news">
+          <div class="col-lg-6 col-md-6">
+            <div class="item">
+              <a href="news-detail.php" class="thumbnail">
+                <img src="images/ex/thumbnail-1.jpg" alt="">
+              </a>
+              <div class="info">
+                <h3  class="tit"><a href="news-detail.php">Sed ut perspiciatis unde omnis iste </a></h3>
+                <p class="home-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6">
+            <div class="item">
+              <a href="news-detail.php" class="thumbnail">
+                <img src="images/ex/thumbnail-1.jpg" alt="">
+              </a>
+              <div class="info">
+                <h3  class="tit"><a href="news-detail.php">Sed ut perspiciatis unde omnis iste </a></h3>
+                <p class="home-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="item">
+              <a href="news-detail.php" class="thumbnail">
+                <img src="images/ex/thumbnail-2.jpg" alt="">
+              </a>
+              <div class="info">
+                <h3  class="tit"><a href="news-detail.php">Sed ut perspiciatis unde omnis iste </a></h3>
+                <p class="home-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="item">
+              <a href="news-detail.php" class="thumbnail">
+                <img src="images/ex/thumbnail-2.jpg" alt="">
+              </a>
+              <div class="info">
+                <h3  class="tit"><a href="news-detail.php">Sed ut perspiciatis unde omnis iste </a></h3>
+                <p class="home-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="item">
+              <a href="news-detail.php" class="thumbnail">
+                <img src="images/ex/thumbnail-2.jpg" alt="">
+              </a>
+              <div class="info">
+                <h3  class="tit"><a href="news-detail.php">Sed ut perspiciatis unde omnis iste </a></h3>
+                <p class="home-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section home-video">
+        <h2 class="title">Video và hoạt động mới</h2>
+        <div class="row color-grey">
+          <div class="col-lg-6 col-sm-12 info">
+              <h3 class="tit">Video: Những thủ thuật cần biết với dòng bút máy Twsbi</h3>
+              <div class="home-text">
+                Bút máy TWSBI được ưa chuộng bởi chất lượng tốt, giá thành vừa phải, đi kèm với cơ chế bơm mực piston độc đáo (thường chỉ thấy có ở các loại bút cực đắt tiền). Bút dùng cho nhu cầu viết hàng ngày.
+              </div>
+              <a href="video-detial.php" class="btn btn-primary">Xem thêm</a>
+          </div>
+           <div class="col-lg-6 col-sm-12 video">
+              <img src="images/ex/thumbnail-3.jpg" alt="" class="thumbnail">
+          </div>
+        </div>
+      </div>
+      <div class="section hone-service">
+          <div class="row">
+            <div class="col-lg-3 col-md-6">
+              <div class="item ship">
+                <div class="col" style="width: 1px;">
+                  <span class="icon"></span>
+                </div>
+                <div class="col text">
+                  <strong>Miễn phí giao hàng</strong>
+                  <p>Đơn hàng 550k (toàn quốc)</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="item pay">
+                <div class="col" style="width: 1px;">
+                  <span class="icon"></span>
+                </div>
+                <div class="col text">
+                  <strong>THANH TOÁN TIỀN LỢI</strong>
+                  <p>Nhận hàng trả tiền (Ship COD) toàn quốc</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="item change">
+                <div class="col" style="width: 1px;">
+                  <span class="icon"></span>
+                </div>
+                <div class="col text">
+                  <strong>5 NGÀY ĐỔI TRẢ</strong>
+                  <p>Đơn hàng 550k (toàn quốc)</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="item hotline">
+                <div class="col" style="width: 1px;">
+                  <span class="icon"></span>
+                </div>
+                <div class="col text">
+                  <strong>Hotline: 091 415 9319</strong>
+                  <p>Từ 8:00 - 22:00 (kể cả CN, ngày lễ)</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      <div class="section home-products">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <a href="#">
+                  <img class="card-img-top" src="images/ex/thumbnail-4.jpg" alt="">
+              </a>
+              <div class="card-body">
+                <h4><a class="card-title" href="product-detail.php">Aenean commodo ligula</a></h4>
+                <div class="rate">
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star-half-o"></span>
+                  <span class="fa fa-star-o"></span>
+                  <span class="scores">(5.3)</span>
+                </div>
+                <p class="card-text price">250.000 Đ</p>
+                <a href="#" class="btn btn-primary">Mua hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center"><a href="#" class="view-more">Xem thêm</a></div>
+      </div>
     </main><!-- /.container -->
-
-
-
+    <footer class="footer">
+      <div class="row">
+        <div class="col-lg-3 info">
+          <a href="#" class="logo"><img src="images/logo-2.png" alt=""></a>
+          <table>
+            <tr>
+              <td>Trụ sở:</td><td><a href="https://www.google.com/maps/place/18+Nguy%E1%BB%85n+C%E1%BB%ADu+V%C3%A2n,+ph%C6%B0%E1%BB%9Dng+17,+B%C3%ACnh+Th%E1%BA%A1nh,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam/@10.795689,106.7056353,17z/data=!3m1!4b1!4m5!3m4!1s0x317528b112f9339d:0x73124c3404a37d36!8m2!3d10.795689!4d106.707824" target="_blank">18/6C Nguyễn Cửu Vân, P. 17, Q. Bình Thạnh, TP.HCM</a></td>
+            </tr>
+            <tr>
+              <td>Hotline:</td><td><a href="tel:0914159319">0914159319</a></td>
+            </tr>
+            <tr>
+              <td>Email:</td><td> <a href="mailto:aas.cskh@gmail.com">aas.cskh@gmail.com</a></td>
+            </tr>
+          </table>
+        </div>
+        <div class="col-lg-3 about">
+          <h4 class="title">Giới thiệu</h4>
+          <ul>
+            <li><a href="#">Về ArtArt</a></li>
+            <li><a href="#">Tuyển dụng</a></li>
+            <li><a href="#">Liên hệ</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 about">
+          <h4 class="title">HỖ TRỢ</h4>
+          <ul>
+            <li><a href="#">Chính sách bảo hành</a></li>
+            <li><a href="#">Qui định đổi trả</a></li>
+            <li><a href="#">Giao hàng</a></li>
+            <li><a href="#">Thanh toán</a></li>
+            <li><a href="#">Câu hỏi thường gặp</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
